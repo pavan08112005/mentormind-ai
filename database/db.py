4edfd887012bf171ab3d543061d3cbf7392cae1c
pavan08112005 <pavankumar462104@gmail.com>
2026-08-16 15:02:53 +0530

    Fix Aiven MySQL connection

--- database/db.py
+++ database/db.py
@@ -6,11 +6,15 @@ load_dotenv()
 
 
 def get_db_connection():
+
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME", "mentormind_ai")
+        database=os.getenv("DB_NAME", "mentormind_ai"),
+        ssl_disabled=False,
+        connection_timeout=10
     )
 
     return connection
